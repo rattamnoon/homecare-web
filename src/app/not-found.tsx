2d4b0c64bc4b9742306f3d67a76abba76a6e88be
UN <rattamnoon.kir@gmail.com>
2025-06-08 20:12:04 +0700

    Add NotFound page component and update theme configuration
    - Introduced a new NotFound component with a styled layout and a button to navigate back to the home page.
    - Updated themeConfig by removing unused theme algorithms to streamline the configuration.

---
+++ src/app/not-found.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { withTheme } from "@/components/layout/MainLayout";
+import { Routes } from "@/constant/routes";
+import styled from "@emotion/styled";
+import { Button, Result } from "antd";
+import { useRouter } from "nextjs-toploader/app";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100svh;
+  background-image: url("/images/login-bg.png");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
+
+export default function NotFound() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(Routes.Home);
+  };
+
+  return withTheme(
+    <Container>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Button type="primary" onClick={handleClick}>
+            Back Home
+          </Button>
+        }
+      />
+    </Container>
+  );
+}
