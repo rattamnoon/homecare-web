8c5c986168598fa969779ad443576b43fcf3797c
UN <rattamnoon.kir@gmail.com>
2025-06-08 22:27:19 +0700

    Update ESLint configuration to ignore generated GraphQL files
    - Removed the rule disabling '@typescript-eslint/no-explicit-any' and added an ignores entry for generated GraphQL files in the ESLint configuration.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,9 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-    },
+    ignores: ["src/gql/generated/**/*"],
   },
 ];
 
